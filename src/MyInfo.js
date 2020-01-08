fb93c44b9c068cff3ed48237b06d8949e2fec9cf
Martin Hanzlik <hanzlik.martin.92@gmail.com>
2020-01-08 01:26:50 +0100

    initial commit

---
+++ src/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+const MyInfo = () => {
+  return (
+    <div className="App">
+      <h1>Martin</h1>
+      <p>A bit info about me...</p>
+      <ul>
+        <li>Norway</li>
+        <li>Switzerland</li>
+        <li>Austria</li>
+      </ul>
+    </div>
+  );
+};
+export default MyInfo;
